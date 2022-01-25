60ab8435ab6cae32805fcdcc7cfdba9d677314fe
PaulGranger <paul.granger@epita.fr>
2022-01-25 12:50:56 +0100

    application identifier filter

---
+++ src/main/java/fr/lernejo/todo/ApplicationIdentifierFilter.java
@@ -0,0 +1,25 @@
+package fr.lernejo.todo;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+public class ApplicationIdentifierFilter implements Filter {
+
+    protected String UUID;
+
+    public ApplicationIdentifierFilter() {
+        UUID uuid = java.util.UUID.randomUUID();
+        this.UUID = uuid.toString();
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+        httpServletResponse.setHeader("Instance-Id", this.UUID);
+    }
+}
